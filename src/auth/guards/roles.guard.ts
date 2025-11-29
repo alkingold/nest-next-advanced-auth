b68f50af7032ef61c93f302596e3b2c96aa068b8
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-11-29 14:37:17 +0100

    feat: add protected profile and by-id user routes

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,37 @@
+import { ROLES_KEY } from '../decorators/roles.decorator';
+
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from '@prisma/generated/enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  public constructor(private readonly reflector: Reflector) {}
+
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new UnauthorizedException(
+        'Access denied: insufficient permissions',
+      );
+    }
+
+    return true;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import { UserRole } from '@prisma/generated/enums';
+import { Authorization } from '@src/auth/decorators/auth.decorator';
+import { Authorized } from '@src/auth/decorators/authorized.decorator';
 
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Authorization()
+  @Get('profile')
+  @HttpCode(HttpStatus.OK)
+  public async findProfile(@Authorized('id') userId: string) {
+    return this.userService.findById(userId);
+  }
+
+  @Authorization(UserRole.ADMIN)
+  @Get('by-id/:id')
+  @HttpCode(HttpStatus.OK)
+  public async findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
 }
