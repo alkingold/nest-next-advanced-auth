22f568a4d148a6e97b0c747482c2ea3ec1186d9c
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-12-26 23:53:40 +0100

    feat: implement update user profile flow

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { hash } from 'argon2';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { AuthMethod } from '@prisma/generated/enums';
 import { PrismaService } from '@src/prisma/prisma.service';
+import { UpdateUserDto } from '@src/user/dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -61,5 +62,22 @@ export class UserService {
     });
 
     return user;
+  }
+
+  public async update(userId: string, dto: UpdateUserDto) {
+    const user = await this.findById(userId);
+
+    const updatedUser = await this.prismaService.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        email: dto.email,
+        displayName: dto.name,
+        isTwoFactorEnabled: dto.isTwoFactorEnabled,
+      },
+    });
+
+    return updatedUser;
   }
 }
