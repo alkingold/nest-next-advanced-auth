e7b6d100a8f8445627c92289b71b56d26a2bf5a9
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-12-27 21:20:44 +0100

    chore: monorepo structure: move nest backend to backend folder

--- backend/src/auth/decorators/authorized.decorator.ts
+++ backend/src/auth/decorators/authorized.decorator.ts
@@ -1,5 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { User } from '@prisma/generated/client';
+import { User } from '@prisma/generated/browser';
 
 export const Authorized = createParamDecorator(
   (data: keyof User, ctx: ExecutionContext) => {
