77decad6847c61cf4cbc454d30807f65547215da
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2025-12-29 10:52:09 +0100

    feat: configure main page and add theme provider

--- frontend/shared/providers/MainProvider.tsx
+++ frontend/shared/providers/MainProvider.tsx
@@ -3,11 +3,23 @@
 import { type ReactNode } from 'react';
 
 import { TanstackQueryProvider } from './TanstackQueryProvider';
+import { ThemeProvider } from './ThemeProvider';
 
 export function MainProvider({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  return <TanstackQueryProvider>{children}</TanstackQueryProvider>;
+  return (
+    <TanstackQueryProvider>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='light'
+        disableTransitionOnChange
+        storageKey='full-auth-theme'
+      >
+        {children}
+      </ThemeProvider>
+    </TanstackQueryProvider>
+  );
 }
