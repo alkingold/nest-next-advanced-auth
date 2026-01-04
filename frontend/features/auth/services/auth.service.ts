55024943b72fdb122fd5eac68458d908d17c1523
Alexandra Bogdanova <alexandra.kaichev@gmail.com>
2026-01-04 16:11:32 +0100

    feat: create auth service to handle authentication requests

---
+++ frontend/features/auth/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { LoginDto, RegisterDto } from '@/features/auth/dtos';
+import { IUser } from '@/features/auth/types/user.types';
+
+import { api } from '@/shared/api';
+
+class AuthService {
+  public async register(body: RegisterDto, recaptcha?: string) {
+    const headers = recaptcha
+      ? {
+          'X-Recaptcha-Token': recaptcha,
+        }
+      : undefined;
+
+    return api.post<IUser, RegisterDto>('/auth/register', body, { headers });
+  }
+
+  public async login(body: LoginDto, recaptcha?: string) {
+    const headers = recaptcha
+      ? {
+          'X-Recaptcha-Token': recaptcha,
+        }
+      : undefined;
+
+    return api.post<IUser, LoginDto>('/auth/login', body, { headers });
+  }
+
+  public async logout() {
+    return api.post('/auth/logout');
+  }
+}
+
+export const authService = new AuthService();
